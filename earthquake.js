438f91174331bacd8f9dfbc0afcdf6cc854d19b8
Debby Hsu <yjhsu@umich.edu>
2017-12-03 02:58:07 -0500

    update earthquake , remove previous earthquake and  draw new

---
+++ earthquake.js
@@ -0,0 +1,35 @@
+  // function level(magnitude) {
+
+  //   if (magnitude<6) {
+  //       // console.log("level1")
+  //       className = "level1";
+  //       return className;
+    
+  //   } else if (magnitude>=6 && magnitude<6.5) {
+  //       // console.log("level2") 
+  //       className = "level2";
+  //       return className;
+
+  //   } else if (magnitude>=6.5 && magnitude<7) {
+  //       // console.log("level3") 
+  //       className = "level3";
+  //       return className;
+
+  //   } else if (magnitude>=7 && magnitude<7.5) {
+  //       // console.log("level4") 
+  //       className = "level4";
+  //       return className;
+
+  //   } else if (magnitude>=7.5 && magnitude<8) {
+  //       // console.log("level5") 
+  //       className = "level5";
+  //       return className;
+
+  //   } else{
+  //       // console.log("level6") 
+  //       className = "level6";
+  //       return className;
+
+  //   }
+
+  // }
